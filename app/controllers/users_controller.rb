39466dbd1771c235e4062f3db445d633487b1d12
lochuynh2613 <loc.h2613@gmail.com>
2018-01-22 11:30:31 +0700

    code extjs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
+    respond_to do |format|
+      format.html {render :index}
+      format.json {render json: @users}
+    end
   end
 
   # GET /users/1
